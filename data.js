ee02aaf40daaf1b57fdee2a2b7168791f111c2b3
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-20 16:02:05 +0100

    Add '.00' to Google Pixel data premium

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 let data = [
   { phone: 'iPhoneX', premium: '50.42 '},
-  { phone: 'GooglePixel', premium: '40' },
+  { phone: 'GooglePixel', premium: '40.00' },
   { phone: 'SamsungGalaxy', premium: '45.99' }
 ]
 
