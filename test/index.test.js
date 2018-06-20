71e9be7b953579a23581fbfaa84ecde6025ae0f4
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-20 11:01:43 +0100

    Add dropdown menu to index with lists of phones to insure

--- test/index.test.js
+++ test/index.test.js
@@ -15,4 +15,15 @@ describe('Index', function(){
       res.text.should.contain('Welcome to Brolly')
     })
   })
+
+  it('should show a list of devices', function(){
+    chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(200)
+      res.text.should.contain('iPhone X 64GB')
+      res.text.should.contain('Google Pixel 2 64GB')
+      res.text.should.contain('Samsung Galaxy S9')
+    })
+  })
 })
