a6de0ee61686046ff6cedc00c27d166097666c52
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-20 10:25:53 +0100

    modify test to expect welcome text on index.ejs

--- test/index.test.js
+++ test/index.test.js
@@ -7,11 +7,12 @@ let expect = chai.expect
 chai.use(chaiHttp)
 
 describe('Index', function(){
-  it('should not error', function(){
+  it('should render welcome text', function(){
     chai.request(server)
     .get('/')
     .end(function(err, res){
       res.should.have.status(200)
+      res.text.should.contain('Welcome to Brolly')
     })
   })
 })
